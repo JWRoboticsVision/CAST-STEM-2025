13ec15408cd5fada83cba6200f2d01f234e082f3
Jikai Wang <jikai.wang@utdallas.edu>
2025-06-26 14:43:31 -0500

    add all contents for Week 4

--- notebooks/08_FoundationPoseROS_answer.py
+++ notebooks/08_FoundationPoseROS_answer.py
@@ -1,7 +1,5 @@
-from pathlib import Path
 import numpy as np
 import cv2
-from matplotlib import colormaps
 import open3d as o3d
 
 import rospy
@@ -10,46 +8,7 @@ from sensor_msgs.msg import Image, CameraInfo
 from image_geometry import PinholeCameraModel
 
 from fetch_grasp.utils import PROJECT_ROOT
-from fetch_grasp.utils.commons import write_rgb_image, write_depth_image
-
-
-def colorize_depth_image(depth_image, colormap_name="jet"):
-    """Colorizes a depth image using a matplotlib colormap."""
-    valid_mask = (depth_image > 0.1) & (depth_image < 1.0)
-    if not np.any(valid_mask):
-        return np.zeros((*depth_image.shape, 3), dtype=np.uint8)
-    # Normalize valid depth to [0, 1]
-    depth_min = np.nanmin(depth_image[valid_mask])
-    depth_max = np.nanmax(depth_image[valid_mask])
-    depth_norm = np.clip((depth_image - depth_min) / (depth_max - depth_min), 0, 1)
-    # Apply colormap
-    colormap = colormaps.get_cmap(colormap_name)
-    colored_image = colormap(depth_norm)[:, :, :3]  # drop alpha
-    # Convert to uint8 RGB image
-    rgb_image = (colored_image * 255).astype(np.uint8)
-    return rgb_image
-
-
-def deproject_depth_image(depth_img, cam_K):
-    H, W = depth_img.shape
-
-    # Create a meshgrid of pixel coordinates
-    u_coords, v_coords = np.meshgrid(np.arange(W), np.arange(H), indexing="xy")
-    u_coords = u_coords.flatten()
-    v_coords = v_coords.flatten()
-
-    # Flatten the depth image to match the pixel coordinates
-    z_coords = depth_img.flatten()
-
-    # Convert pixel coordinates to normalized image coordinates
-    x_coords = (u_coords - cam_K[0, 2]) * z_coords / cam_K[0, 0]
-    y_coords = (v_coords - cam_K[1, 2]) * z_coords / cam_K[1, 1]
-
-    # Stack the coordinates to form homogeneous coordinates
-    points_3d = np.vstack((x_coords, y_coords, z_coords))  # Shape (3, N)
-    points_3d = points_3d.T  # Shape (N, 3)
-
-    return points_3d
+from fetch_grasp.utils.commons import write_rgb_image, write_depth_image, colorize_depth_image, deproject_depth_image
 
 
 def get_camera_K(caminfo_topic):
